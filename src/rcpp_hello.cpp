b582713a00e7cc3fddadb4a9edd4193c98842965
Drew Tyre <atyre2@unl.edu>
2019-02-01 13:48:57 -0600

    add linked, landscpe, habitat

--- src/rcpp_hello.cpp
+++ src/rcpp_hello.cpp
@@ -6,49 +6,10 @@ using namespace Rcpp;
 #include "matrices.h"
 
 
-// This is a simple function using Rcpp that creates an R list
-// containing a character vector and a numeric vector.
-//
-// Learn more about how to use Rcpp at:
-//
-//   http://www.rcpp.org/
-//   http://adv-r.had.co.nz/Rcpp.html
-//
-// and browse examples of code using Rcpp at:
-//
-//   http://gallery.rcpp.org/
-//
-
-// [[Rcpp::export]]
-List rcpp_hello() {
-  CharacterVector x = CharacterVector::create("foo", "bar");
-  NumericVector y   = NumericVector::create(0.0, 1.0);
-  List z            = List::create(x, y);
-  return z;
-}
 
 // [[Rcpp::export]]
 float one(){
   return gasdev();
-}
-
-
-// [[Rcpp::export]]
-// float abigone(){
-//   float **bigone;
-//   bigone = matrix(0,10,0,10);
-//
-//   return bigone;
-// }
-
-// [[Rcpp::export]]
-double sumC(NumericVector x) {
-  int n = x.size();
-  double total = 0;
-  for(int i = 0; i < n; ++i) {
-    total += x[i];
-  }
-  return total;
 }
 
 
