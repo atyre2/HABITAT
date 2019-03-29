ce2d2939e19b33d3a6043753adfdcf0c840ee950
Drew Tyre <atyre2@unl.edu>
2019-03-29 14:21:34 -0500

    NOW IT WORKS WTF

--- src/habitat.cpp
+++ src/habitat.cpp
@@ -962,18 +962,17 @@ void MakeDTrnsfrmTable(Parameters *Pm, float HabitatPivot){
     float curdeathrate;
     float temp;
     int i,j;
-  printf("Entering MakeDTrnsfrmTable()");
     if (Pm->SurvSlope > 0.0001)
    {
-        // for(i=0;i<=Pm->MaxAgeClass;i++){
-        //     curdeathrate = (float) log((double) Pm->DeathRate[i]/(1-Pm->DeathRate[i]));
-        //     for(j=0;j<=MAXHAB;j++){
-        //         temp = -Pm->SurvSlope*(((float)j/HabitatPivot) - 1);    /* habitat values less than AVGHAB are positive */
-        //         temp += curdeathrate;           /* add in the transformed survival rate */
-        //         temp = (float) exp((double) temp);                  /* begin backtransformation */
-        //         DTable[i][j] = temp / (temp + 1);   /* complete backtransformation and store */
-        //     }
-        // }
+        for(i=0;i<=Pm->MaxAgeClass;i++){
+            curdeathrate = (float) log((double) Pm->DeathRate[i]/(1-Pm->DeathRate[i]));
+            for(j=0;j<=MAXHAB;j++){
+                temp = -Pm->SurvSlope*(((float)j/HabitatPivot) - 1);    /* habitat values less than AVGHAB are positive */
+                temp += curdeathrate;           /* add in the transformed survival rate */
+                temp = (float) exp((double) temp);                  /* begin backtransformation */
+                DTable[i][j] = temp / (temp + 1);   /* complete backtransformation and store */
+            }
+        }
    }
    else
     {
@@ -984,7 +983,6 @@ void MakeDTrnsfrmTable(Parameters *Pm, float HabitatPivot){
         }
     }
 
-    printf("leaving MakeDTrnsfrmTable()");
     return;
 }
 //
